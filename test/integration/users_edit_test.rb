3c8e66cf50f97a9779fd0daefc397a0eb5717cc7
Ryu Sato <ryu@weseek.co.jp>
2018-10-09 02:32:56 +0900

    User の更新処理を行うロジックとテストを追加

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -16,4 +16,21 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_template 'users/edit'
     assert_select 'div.alert', 'The form contains 4 errors.'
   end
+
+  test "successful edit" do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    name = 'bar'
+    email = 'foo@valid.domain'
+    patch user_path(@user), params: { user: { name:  name,
+                                              email: email,
+                                              password: '',
+                                              password_confirmation: '' } }
+
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal @user.name, name
+    assert_equal @user.email, email
+  end
 end
