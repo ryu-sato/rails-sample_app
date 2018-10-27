53a195b62c2b979a863485b9e375c4315eb20e39
Ryu Sato <ryu@weseek.co.jp>
2018-10-27 19:34:41 +0900

    Userモデル削除時に関連するMicropostモデルを削除する。Micropostモデルの検索オーダーをdefaultで最新の投稿順に設定。

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   before_save :downcase_email
   has_secure_password
 
-  has_many :microposts
+  has_many :microposts, dependent: :destroy
   
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :name,  presence: true, length: { maximum: 50  }
