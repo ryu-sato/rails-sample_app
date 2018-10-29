6f9b4bf5567537559f8cfb125cefe2dd2efbf0c0
Ryu Sato <ryu@weseek.co.jp>
2018-10-29 15:24:12 +0900

    picture投稿フォームにvalidationを追加

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -5,6 +5,14 @@ class Micropost < ApplicationRecord
 
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+  validate  :picture_size
 
   default_scope -> { order(created_at: :desc) }
+
+  private
+    def picture_size
+      if picture.size > 5.megabytes
+        errors.add(:picture, "should be less than 5MB")
+      end
+    end
 end
